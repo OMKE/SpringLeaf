98238c5fa2f99a3eea6a85008663a3b0dfcad37a
Omar Iriskic <noisewavehd@gmail.com>
2020-06-12 15:01:21 +0200

    Added getter for handler name, needed for referencing

--- utils/handlers/project_structure_handler.py
+++ utils/handlers/project_structure_handler.py
@@ -1,10 +1,25 @@
 
+from ..file_handler import FileHandler
 from .handler import Handler
 
 
 class ProjectStructureHandler(Handler):
-    def __init__(self):
-        super().__init__()
 
     def handle(self, *args):
-        print(args)
+        if args[0] == "Custom":
+            self.use_custom_project_structure()
+        else:
+            structure = FileHandler.get_project_structure(args[0])
+            FileHandler.create_config_file({
+                "springleaf": {
+                    "project": {
+                        "structure": structure["name"]
+                    }
+                }
+            })
+
+    def name(self):
+        return "structure"
+
+    def use_custom_project_structure(self):
+        pass
