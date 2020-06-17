823a1c44f0c9ad3dfe4bf9214f2c0261fd7dd40f
Omar Iriskic <noisewavehd@gmail.com>
2020-06-17 19:23:09 +0200

    Added functionality for generator

--- springleaf/generator.py
+++ springleaf/generator.py
@@ -1,17 +1,30 @@
 
+import jinja2
 
-
-
+from springleaf.utils.exceptions import (GeneratorFileExistsException,
+                                         GeneratorFileNameNotFoundException,
+                                         GeneratorPathNotFoundException,
+                                         TemplateDataNotFoundException,
+                                         TemplateNotFoundException)
+from springleaf.utils.file_handler import FileHandler
 from springleaf.utils.java_parser import JavaParser
 
 
 class Generator:
     def __init__(self):
         self.java_parser = JavaParser()
+        self.output = None
 
     def generate(self):
-        pass
+        if FileHandler.exists(self.path + "/" + self.name):
+            raise GeneratorFileExistsException
+        if self.path is None:
+            raise GeneratorPathNotFoundException
+        if self.name is None:
+            raise GeneratorFileNameNotFoundException
 
+        with open(self.path + self.name, "w") as file:
+            file.write(self.output)
     """
     #TODO
     @desc:
@@ -20,3 +33,30 @@ class Generator:
 
     def select_directory_structure(self):
         pass
+
+    def set_template(self, name):
+        self.template = jinja2.Template(
+            FileHandler.get_template_file(name))
+        return self
+
+    def set_data(self, data):
+        self.data = data
+        return self
+
+    def render(self):
+        if self.data is None:
+            raise TemplateDataNotFoundException
+        if self.template is None:
+            raise TemplateNotFoundException
+
+        self.output = self.template.render(data=self.data)
+
+        return self
+
+    def set_path(self, path):
+        self.path = path
+        return self
+
+    def set_name(self, name):
+        self.name = name
+        return self
