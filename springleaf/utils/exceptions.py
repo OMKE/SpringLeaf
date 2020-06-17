2d5c514895310d2cf1db56ff10c301b848c88582
Omar Iriskic <noisewavehd@gmail.com>
2020-06-17 19:18:36 +0200

    Added exceptions

--- springleaf/utils/exceptions.py
+++ springleaf/utils/exceptions.py
@@ -2,3 +2,23 @@
 
 class QuestionNotCreatedException(Exception):
     pass
+
+
+class TemplateNotFoundException(Exception):
+    pass
+
+
+class TemplateDataNotFoundException(Exception):
+    pass
+
+
+class GeneratorPathNotFoundException(Exception):
+    pass
+
+
+class GeneratorFileNameNotFoundException(Exception):
+    pass
+
+
+class GeneratorFileExistsException(Exception):
+    pass
