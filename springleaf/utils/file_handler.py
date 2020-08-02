8b422e0693de489befe1b4f18a368d1d99ff1726
Omar Iriskic <noisewavehd@gmail.com>
2020-08-02 02:02:48 +0200

    Added response to the config file check

--- springleaf/utils/file_handler.py
+++ springleaf/utils/file_handler.py
@@ -331,7 +331,7 @@ class FileHandler:
     def validate_config_file():
         is_valid = True
         config_attributes = ["build", 'name', 'package',
-                             'structure', 'entities-folder', 'methods', 'controller-type']
+                             'structure', 'entities-folder', 'methods', 'controller-type', 'response']
         with open(FileHandler.current_dir() + "/springleaf.yaml") as file:
             config = yaml.load(file, Loader=yaml.FullLoader)
             for i in config_attributes:
