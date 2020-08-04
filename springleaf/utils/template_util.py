b1fbd3ffc3df07e41e3031e97d2c9b513a26ef51
Omar Iriskic <noisewavehd@gmail.com>
2020-08-04 18:54:54 +0200

    Added check for service

--- springleaf/utils/template_util.py
+++ springleaf/utils/template_util.py
@@ -1,4 +1,6 @@
 
+from .file_handler import FileHandler
+
 
 """
 TemplateUtil
@@ -20,7 +22,7 @@ class TemplateUtil:
         entity_name - name of the entity class
     """
 
-    def __init__(self, name: str, template_name: str, data: object, methods: str, package: str, controller: str, response: str, entity_name: str):
+    def __init__(self, name: str, template_name: str, data: object, methods: str, package: str, controller: str, response: str, entity_name: str, autowire: str):
         self.name = name
         self.template_name = template_name
         self.data = data
@@ -32,3 +34,19 @@ class TemplateUtil:
             ".")[-1] if "." in response else "ResponseEntity"
         self.path = "./src/main/java/" + self.package.replace(".", "/") + "/"
         self.entity_name = entity_name
+        self.autowire = autowire
+        self.service_check()
+
+    """
+    service_check
+    @desc:
+        If file is Service type, we want to create folder in which it will hold two files, interface and implementation
+    @return: void - 
+    """
+
+    def service_check(self):
+        if "&" in self.path and "Service" == self.template_name or "ServiceImpl" == self.template_name:
+            self.path = self.path.replace(
+                "&", (self.name.replace("Service", "")).replace("Impl", "").lower())
+            self.package = self.package.replace(
+                "&", (self.name.replace("Service", "")).replace("Impl", "").lower())
